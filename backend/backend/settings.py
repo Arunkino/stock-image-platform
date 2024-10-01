426fa65801e63be8f8347dbe37f71f426a17db19
Arun KS <arunsubrahmanyan@gmail.com>
2024-10-01 10:53:36 +0530

    Backend for images crud

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +157,7 @@ REST_FRAMEWORK = {
 
 # to use custom user model
 AUTH_USER_MODEL = 'users.CustomUser'
+
+# Configure media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- backend/images/views.py
+++ backend/images/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Image
+from .serializers import ImageSerializer
 
-# Create your views here.
+class ImageViewSet(viewsets.ModelViewSet):
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Image.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
